e206d3e932e595a618aeea28adddef1a6c67ff74
kbradley91 <kbradley09@gmail.com>
2015-12-02 14:58:31 -0500

    asdf

---
+++ Debug/RFFT_f32_sincostable.pp
@@ -0,0 +1,17 @@
+# FIXED
+
+RFFT_f32_sincostable.obj: ../RFFT_f32_sincostable.c
+RFFT_f32_sincostable.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/math.h
+RFFT_f32_sincostable.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/linkage.h
+RFFT_f32_sincostable.obj: C:/ti/controlSUITE/libs/dsp/FPU/v1_50_00_00/include/fpu_rfft.h
+RFFT_f32_sincostable.obj: C:/ti/controlSUITE/libs/dsp/FPU/v1_50_00_00/include/fpu_types.h
+RFFT_f32_sincostable.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdint.h
+RFFT_f32_sincostable.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/float.h
+
+../RFFT_f32_sincostable.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/math.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/linkage.h: 
+C:/ti/controlSUITE/libs/dsp/FPU/v1_50_00_00/include/fpu_rfft.h: 
+C:/ti/controlSUITE/libs/dsp/FPU/v1_50_00_00/include/fpu_types.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-c2000_6.4.2/include/float.h: 
--- main.c
+++ main.c
@@ -6,11 +6,28 @@
 #include "Lab9.h"
 #include "FPU.h"
 #include <math.h>
+void fft_init();
 
 RFFT_F32_STRUCT fft;
 
 
-void fft_init();
+#define FFT_SIZE 256
+#define FFT_STAGES 8  //log base 2 (FFT_size)
+
+//#pragma DATA_SECTION(sampleBuffer, "INBUFA");
+static float sampleBuffer[FFT_SIZE];
+static float outBuffer[FFT_SIZE];
+float TwiddleBuffer[FFT_SIZE];
+static float MagBuffer[FFT_SIZE/2];
+static float SumSpectrum[FFT_SIZE/2];
+
+static int sampleCount = 0;
+static int sampleBufferFull = 0; //variable used to denote whether the buffer has been filled
+
+interrupt void sampleGet_ISR(void);
+void initBuffers();
+
+
 unsigned long ISRvalue = &sampleGet_ISR;
 
 
@@ -35,22 +52,70 @@ int main(void) {
 
 
 	while(1){
+		if(sampleBufferFull){
+			fft.InBuf = &sampleBuffer[0];
+			int i;
+			for(i = 0;i<FFT_SIZE;i++){
+				outBuffer[i] = 0;
+			}
+			for(i=0;i<FFT_SIZE/2;i++){
+				MagBuffer[i] = 0;
+			}
+			RFFT_f32(&fft);
+			//fft.MagBuf = &sampleBuffer[0];
+			RFFT_f32_mag(&fft);
+
+			sampleBufferFull = 0;
+			EINT;
+		}
+		else{
+			//do nothing
+		}
 
 	}
 	return 0;
 }
 
 void fft_init(){
-	fft.InBuf = sampleBuffer;
-	fft.OutBuf = outBuffer;
-	fft.CosSinBuf = TwiddleBuffer;
+	fft.InBuf = &sampleBuffer[0];
+	fft.OutBuf = &outBuffer[0];
+	fft.CosSinBuf = &TwiddleBuffer[0];
 	fft.FFTSize = FFT_SIZE;
 	fft.FFTStages = FFT_STAGES;
-	fft.MagBuf = MagBuffer;
+	fft.MagBuf = &MagBuffer[0];
 	RFFT_f32_sincostable(&fft);
-
 
 }
 
+interrupt void test_ISR(void){
+	unsigned int input = ADC_get();
+	DAC_set(input);
+}
+
+interrupt void sampleGet_ISR(void){
+	unsigned int input = ADC_get();
+	DAC_set(input);
+	sampleBuffer[sampleCount] = (float)input;
+
+	sampleCount++;
+	if(sampleCount == FFT_SIZE){
+		sampleCount = 0;
+		sampleBufferFull = 1;
+		DINT; //disables interrupts, if doesn't work can send to dummy interrupt vector
+	}
+
+}
+
+void initBuffers(){
+	int i,k;
+	for(i = 0; i<FFT_SIZE;i++){
+		sampleBuffer[i] = 0;
+	}
+
+	for(k = 0; i<FFT_SIZE/2;i++){
+			SumSpectrum[i] = 0;
+	}
+
+}
 
 
